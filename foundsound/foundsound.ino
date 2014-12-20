887f4f8ad59eaf51103f4d23c3f3a8b987652985
cdamon17 <cdamon17@MacBookAir-48D705C89FBB-2.local>
2014-12-19 14:34:16 -1000

    dots in an array with no sound
    can put the dots x and y variables into an array, but no sound will
    come out, will need to figure that out

--- foundsound/foundsound.ino
+++ foundsound/foundsound.ino
@@ -1,15 +1,47 @@
 #include <MeggyJrSimple.h>
 // global variables go here
-int levelArray[3] = {0, 0, 0}; // creates the different levels for the game
-                              // and updates the levels
-int completionArray[3] = {0, 0, 0}; // checks what player did to what is
-                                  // needed to move on to the next level
 int xcoord = 0;
 int ycoord = 0;
+
+int C3x = 4;
+int C3y = 5;
+int B3x = 6;
+int B3y = 2;
+int A3x = 2;
+int A3y = 3;
+int D3x = 0;
+int D3y = 4;
+
+struct Point
+{
+  /*int x;
+  int y;*/
+  int C3x;
+  int C3y;
+  int B3x;
+  int B3y;
+  int A3x;
+  int A3y;
+  int D3x;
+  int D3y;
+  
+  
+  //boolean visited;
+};
+
+Point p1={C3x,C3y};
+Point p2={B3x,B3y};
+Point p3={A3x,A3y};
+Point p4={D3x,D3y};
+
+Point dotArray[64]={p1,p2,p3,p4};
+
+int marker=4;
 
 void setup()
 {
   MeggyJrSimpleSetup();
+  Serial.begin(9600); // code to show the points of variables
 }
 
 
@@ -18,7 +50,10 @@ void setup()
 void loop() // loop code that goes through loop
 {
   shift();
+  DrawPx(p1.C3x,p1.C3y,Violet);
   DrawPx(xcoord,ycoord,Blue); // draw dot
+  dotsound(); // make sounds from dots
+
   /*
   drawlevel
   updatelevel
@@ -56,9 +91,6 @@ void shift() // taken from Maze_Game of Brennan Brown
       if (xcoord < 7)
       xcoord = xcoord + 1; // indented because belongs to inner if statement;
                            //  could also say xcoord++
-        if (ReadPx(xcoord, ycoord) == Green)
-          xcoord = xcoord -1; // as soon as user lands on green pixel,
-                          // go +1 pixels to original position
       if (xcoord == 7)
         Tone_Start (ToneC3,100);
     }
@@ -67,8 +99,6 @@ void shift() // taken from Maze_Game of Brennan Brown
     {
       if (xcoord > 0)
       xcoord = xcoord - 1;
-      if (ReadPx(xcoord, ycoord) == Green)
-      xcoord = xcoord +1;
       if (xcoord == 0)
       Tone_Start (ToneC3,100);
     }
@@ -77,9 +107,6 @@ void shift() // taken from Maze_Game of Brennan Brown
     {
       if (ycoord < 7)
       ycoord = ycoord + 1;
-      if (ReadPx(xcoord, ycoord) == Green)
-      ycoord = ycoord -1; // as soon as user lands on green pixel,
-                          // go -1 pixels to original position
       if (ycoord == 7)
       Tone_Start (ToneC3,100); // play error sound when go off screen
     }
@@ -88,10 +115,21 @@ void shift() // taken from Maze_Game of Brennan Brown
     {
       if (ycoord > 0)
       ycoord = ycoord - 1;
-      if (ReadPx(xcoord, ycoord) == Green)
-      ycoord = ycoord +1;
       if (ycoord == 0)
       Tone_Start (ToneC3,100); // play error sound when go off screen
     }
+}
+
+void dotsound() // simple code to make dots create sound
+{
+ CheckButtonsDown();
+ if (Button_A)
+ {
+  if (ReadPx(xcoord == dotArray[0].C3x,ycoord == dotArray[0].C3y) == Violet)
+   {
+     Tone_Start (ToneC3,300);
+   }
+   DrawPx(0,0,15);
+ }
 }
 
